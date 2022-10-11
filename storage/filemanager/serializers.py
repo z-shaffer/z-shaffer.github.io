26d9e28ad432733e378475172baa3794116a29db
z-shaffer <68123195+z-shaffer@users.noreply.github.com>
2022-10-11 00:01:01 -0400

    Documentation

--- storage/filemanager/serializers.py
+++ storage/filemanager/serializers.py
@@ -14,9 +14,13 @@ class DataSerializer(serializers.ModelSerializer):
     filetype = serializers.SerializerMethodField()
     since_added = serializers.SerializerMethodField()
 
+    # Data table frame
+    
     class Meta:
         model = Data
         fields = ('file_id', 'file', 'since_added', 'size', 'name', 'filetype')
+        
+    # Return size of file    
         
     def get_size(self, obj):
         file_size = ''
@@ -24,15 +28,21 @@ class DataSerializer(serializers.ModelSerializer):
             file_size = obj.file.size
         return file_size
 
+    # Return name of file
+      
     def get_name(self, obj):
         file_name = ''
         if obj.file and hasattr(obj.file, 'name'):
             file_name = obj.file.name
         return file_name
 
+    # Return file extension  
+      
     def get_filetype(self, obj):
       filename = obj.file.name
       return filename.split('.')[-1]
+    
+    # Return upload date
       
     def get_since_added(self, obj):
         date_added = obj.date_created
